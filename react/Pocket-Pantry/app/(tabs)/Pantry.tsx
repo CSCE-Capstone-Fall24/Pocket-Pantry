faae216c9ec143d74799d9f904e7d3dba2ad5bb7
Zack Shawver <zackshawver17@gmail.com>
2024-11-19 15:32:42 -0600

    Scroll view

--- react/Pocket-Pantry/app/(tabs)/Pantry.tsx
+++ react/Pocket-Pantry/app/(tabs)/Pantry.tsx
@@ -1,66 +1,68 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { ScrollView, View, Text, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import PantryItem from '@/components/PantryItem';
 
 export default function Pantry () {
 
   return (
+    <ScrollView>
     <SafeAreaView>
+        <Text style={styles.title}>
+          Pantry
+        </Text>
       
-      <Text style={styles.title}>
-        Pantry
-      </Text>
-      
-      <Text style={styles.header}>
-        Category 1
-      </Text>
-      <View style={styles.line}></View>
-      <PantryItem
-        name="Ingredient1"
-        quantity={10}
-      />
-      <View style={styles.line}></View>
-      <PantryItem
-        name="Ingredient2"
-        quantity={10}
-      />
-      <View style={styles.line}></View>
-      <PantryItem
-        name="Ingredient3"
-        quantity={10}
-      />
-      <View style={styles.line}></View>
-      <PantryItem
-        name="Ingredient4"
-        quantity={10}
-      />
+        <Text style={styles.header}>
+          Category 1
+        </Text>
+        <View style={styles.line}></View>
+        <PantryItem
+          name="Ingredient1"
+          quantity={10}
+        />
+        <View style={styles.line}></View>
+        <PantryItem
+          name="Ingredient2"
+          quantity={10}
+        />
+        <View style={styles.line}></View>
+        <PantryItem
+          name="Ingredient3"
+          quantity={10}
+        />
+        <View style={styles.line}></View>
+        <PantryItem
+          name="Ingredient4"
+          quantity={10}
+        />
 
-      <Text style={styles.header}>
-        Category 2
-      </Text>
-      <View style={styles.line}></View>
-      <PantryItem
-        name="Ingredient5"
-        quantity={10}
-      />
-      <View style={styles.line}></View>
-      <PantryItem
-        name="Ingredient6"
-        quantity={10}
-      />
-      <View style={styles.line}></View>
-      <PantryItem
-        name="Ingredient7"
-        quantity={10}
-      />
-      <View style={styles.line}></View>
-      <PantryItem
-        name="Ingredient8"
-        quantity={10}
-      />
+        <Text style={styles.header}>
+          Category 2
+        </Text>
+        <View style={styles.line}></View>
+        <PantryItem
+          name="Ingredient5"
+          quantity={10}
+        />
+        <View style={styles.line}></View>
+        <PantryItem
+          name="Ingredient6"
+          quantity={10}
+        />
+        <View style={styles.line}></View>
+        <PantryItem
+          name="Ingredient7"
+          quantity={10}
+        />
+        <View style={styles.line}></View>
+        <PantryItem
+          name="Ingredient8"
+          quantity={10}
+        />
        
+      
     </SafeAreaView>
+    </ScrollView>
   );
 };
 
--- react/Pocket-Pantry/app/(tabs)/Shopping.tsx
+++ react/Pocket-Pantry/app/(tabs)/Shopping.tsx
@@ -1,66 +1,67 @@
 import ShoppingItem from '@/components/ShoppingItem';
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { ScrollView, View, Text, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Shopping () {
 
   return (
-    <SafeAreaView>
-      
-      <Text style={styles.title}>
-        Shopping List
-      </Text>
-      
-      <Text style={styles.header}>
-        Category 1
-      </Text>
-      <View style={styles.line}></View>
-      <ShoppingItem
-        name="Item1"
-        quantity={10}
-      />
-      <View style={styles.line}></View>
-      <ShoppingItem
-        name="Item2"
-        quantity={10}
-      />
-      <View style={styles.line}></View>
-      <ShoppingItem
-        name="Item3"
-        quantity={10}
-      />
-      <View style={styles.line}></View>
-      <ShoppingItem
-        name="Item4"
-        quantity={10}
-      />
-
-      <Text style={styles.header}>
-        Category 2
-      </Text>
-      <View style={styles.line}></View>
-      <ShoppingItem
-        name="Item5"
-        quantity={10}
-      />
-      <View style={styles.line}></View>
-      <ShoppingItem
-        name="Item6"
-        quantity={10}
-      />
-      <View style={styles.line}></View>
-      <ShoppingItem
-        name="Item7"
-        quantity={10}
-      />
-      <View style={styles.line}></View>
-      <ShoppingItem
-        name="Item8"
-        quantity={10}
-      />
+    <ScrollView>
+      <SafeAreaView> 
+        <Text style={styles.title}>
+          Shopping List
+        </Text>
         
-    </SafeAreaView>
+        <Text style={styles.header}>
+          Category 1
+        </Text>
+        <View style={styles.line}></View>
+        <ShoppingItem
+          name="Item1"
+          quantity={10}
+        />
+        <View style={styles.line}></View>
+        <ShoppingItem
+          name="Item2"
+          quantity={10}
+        />
+        <View style={styles.line}></View>
+        <ShoppingItem
+          name="Item3"
+          quantity={10}
+        />
+        <View style={styles.line}></View>
+        <ShoppingItem
+          name="Item4"
+          quantity={10}
+        />
+
+        <Text style={styles.header}>
+          Category 2
+        </Text>
+        <View style={styles.line}></View>
+        <ShoppingItem
+          name="Item5"
+          quantity={10}
+        />
+        <View style={styles.line}></View>
+        <ShoppingItem
+          name="Item6"
+          quantity={10}
+        />
+        <View style={styles.line}></View>
+        <ShoppingItem
+          name="Item7"
+          quantity={10}
+        />
+        <View style={styles.line}></View>
+        <ShoppingItem
+          name="Item8"
+          quantity={10}
+        />
+          
+      </SafeAreaView>
+    </ScrollView>
   );
 };
 
