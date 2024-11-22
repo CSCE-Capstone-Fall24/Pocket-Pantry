42cd515d9825170645fea1813529cf501d1f7952
Zack Shawver <zackshawver17@gmail.com>
2024-11-21 23:35:01 -0600

    Ability to add new items to pantry, consistency and clarity changes

--- react/Pocket-Pantry/app/(tabs)/Pantry.tsx
+++ react/Pocket-Pantry/app/(tabs)/Pantry.tsx
@@ -1,40 +1,60 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, ScrollView, TouchableOpacity, Alert } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, TouchableOpacity, Alert, Modal, TextInput, Pressable } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { BlurView } from 'expo-blur';
 import PantryItem from '@/components/PantryItem';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import DateTimePicker from '@react-native-community/datetimepicker'
 
 export default function Pantry () {
   const [items, setItems] = useState([
-    { id: 1, name: "Ingredient1", quantity: 10, unit: "lbs" },
-    { id: 2, name: "Ingredient2", quantity: 10, unit: "lbs" },
-    { id: 3, name: "Ingredient3", quantity: 10, unit: "lbs" },
-    { id: 4, name: "Ingredient4", quantity: 10, unit: "lbs" },
+    { id: 1, name: "Ingredient1", quantity: 10, unit: "lbs", expiration: new Date(), shared: [false, false, false, false] },
+    { id: 2, name: "Ingredient2", quantity: 10, unit: "lbs", expiration: new Date(), shared: [false, false, false, false] },
+    { id: 3, name: "Ingredient3", quantity: 10, unit: "lbs", expiration: new Date(), shared: [false, false, false, false] },
+    { id: 4, name: "Ingredient4", quantity: 10, unit: "lbs", expiration: new Date(), shared: [false, false, false, false] },
   ]);
 
-  const [newItemName, setNewItemName] = useState('');
-  const [newItemQuantity, setNewItemQuantity] = useState('');
-  const [newItemUnit, setNewItemUnit] = useState('');
+  const [isWindowVisible, setWindowVisible] = useState(false);
+  const openWindow = () => setWindowVisible(true);
+  const closeWindow = () => {
+    setWindowVisible(false);
+    setNewName('');
+    setNewQuantity('');
+    setNewUnit('');
+    setNewExpiration(new Date());
+    setNewShared([false, false, false, false]);
+  };
 
-  const [isModalVisible, setModalVisible] = useState(false);
-  const openModal = () => setModalVisible(true);
-  const closeModal = () => {
-    setModalVisible(false);
-    setNewItemName('');
-    setNewItemQuantity('');
-    setNewItemUnit('');
+  const [newName, setNewName] = useState('');
+  const [newQuantity, setNewQuantity] = useState('');
+  const [newUnit, setNewUnit] = useState('');
+  
+  const [newExpiration, setNewExpiration] = useState(new Date());
+  const [isScrollerVisible, setScrollerVisible] = useState(false);
+  const openScroller = () => setScrollerVisible(true);
+  const closeScroller = () => setScrollerVisible(false);
+  
+  const [newShared, setNewShared] = useState([false, false, false, false]);
+  const newSharedToggle = (index: number) => {
+    setNewShared((prevState) =>
+      prevState.map((item, i) => (i === index ? !item : item))
+    );
   };
 
   const addItem = () => {
-    if (newItemName && newItemQuantity && newItemUnit) {
+    if (isNaN(Number(newQuantity))) {
+      Alert.alert('Quantity must be a number.');
+    } else if (newName && newQuantity && newExpiration && newShared) {
       const newItem = {
         id: items.length + 1,
-        name: newItemName,
-        quantity: Number(newItemQuantity),
-        unit: newItemUnit,
+        name: newName,
+        quantity: Number(newQuantity),
+        unit: newUnit,
+        expiration: newExpiration,
+        shared: newShared,
       };
       setItems([...items, newItem]);
-      closeModal();
+      closeWindow();
     } else {
       Alert.alert('Please fill out all fields.');
     }
@@ -46,16 +66,165 @@ export default function Pantry () {
 
         <View style={styles.header}>
           <Text style={styles.title}>Pantry</Text>
-          <TouchableOpacity style={styles.addButton} onPress={addItem}>
+          <TouchableOpacity style={styles.addButton} onPress={openWindow}>
             <Ionicons name="add-outline" size={40} color="white"/>
           </TouchableOpacity>
         </View>
+
+        {/* Add item window */}
+        <Modal
+          transparent={true}
+          animationType="fade"
+          visible={isWindowVisible}
+          onRequestClose={closeWindow}
+        >
+          <BlurView
+            style={StyleSheet.absoluteFill}
+            intensity={20}
+          />
+          <View style={styles.windowAlignment}>
+            <View style={styles.window}>
+              <Text style={styles.windowTitle}>Add item</Text>
+              
+              {/* Input name */}
+              <View style={styles.nameContainer}>
+                <Text style={styles.windowText}>Name:  </Text>
+                <TextInput
+                  style={styles.nameInput}
+                  value={newName}
+                  onChangeText={(value) => setNewName(value)}
+                />
+              </View>
+
+              {/* Input quantity */}
+              <View style={styles.quantityContainer}>
+                <Text style={styles.windowText}>Quantity:  </Text>
+                <TextInput
+                  style={styles.quantityInput}
+                  value={newQuantity}
+                  onChangeText={(value) => setNewQuantity(value)}
+                />
+              </View>
+
+              {/* Input unit */}
+              
+
+              {/* Input expiration date */}
+              <View style={styles.expirationContainer}>
+                <Text style={styles.windowText}>Expiration date:  </Text>
+                <TouchableOpacity
+                  style={styles.expirationInput}
+                  onPress={openScroller}
+                >
+                  <Text style={styles.windowText}>{newExpiration.toLocaleDateString()}</Text>
+                </TouchableOpacity>
+              </View>
+
+              {/* Expiration date scroller */}
+              <Modal
+                transparent={true}
+                animationType="slide"
+                visible={isScrollerVisible}
+                onRequestClose={closeScroller}
+              >
+                <Pressable style={styles.scrollerSpacer} onPress={closeScroller}></Pressable>
+                <View>
+                  <View style={styles.doneButtonContainer}>
+                    <TouchableOpacity onPress={closeScroller}>
+                      <Text style={styles.doneButtonText}>Done</Text>
+                    </TouchableOpacity>
+                  </View>
+                  <View style={styles.scroller}>
+                    {isScrollerVisible && (
+                      <DateTimePicker
+                        value={newExpiration}
+                        mode="date"
+                        display="spinner"
+                        onChange={(event, date) => {
+                          if (date) setNewExpiration(date);
+                        }}
+                      />
+                    )}
+                  </View>
+                </View>
+              </Modal>
+
+              {/* Set item as shared */}
+              <View style={styles.sharedSpacer}>
+                <View style={styles.sharedContainer}>
+                  <Text style={styles.windowText}>Shared with user1:  </Text>
+                  <Pressable onPress={() => newSharedToggle(0)}>
+                    {newShared[0] ? (
+                      <Ionicons name="checkmark-circle" size={32} color="#e167a4"/>
+                    ) : (
+                      <Ionicons name="ellipse-outline" size={32} color="#e167a4"/>
+                    )}
+                  </Pressable>
+                </View>
+
+                <View style={styles.sharedContainer}>
+                  <Text style={styles.windowText}>Shared with user2:  </Text>
+                  <Pressable onPress={() => newSharedToggle(1)}>
+                    {newShared[1] ? (
+                      <Ionicons name="checkmark-circle" size={32} color="#f4737e"/>
+                    ) : (
+                      <Ionicons name="ellipse-outline" size={32} color="#f4737e"/>
+                    )}
+                  </Pressable>
+                </View>
+
+                <View style={styles.sharedContainer}>
+                  <Text style={styles.windowText}>Shared with user3:  </Text>
+                  <Pressable onPress={() => newSharedToggle(2)}>
+                    {newShared[2] ? (
+                      <Ionicons name="checkmark-circle" size={32} color="#ff8667"/>
+                    ) : (
+                      <Ionicons name="ellipse-outline" size={32} color="#ff8667"/>
+                    )}
+                  </Pressable>
+                </View>
+
+                <View style={styles.sharedContainer}>
+                  <Text style={styles.windowText}>Shared with user4:  </Text>
+                  <Pressable onPress={() => newSharedToggle(3)}>
+                    {newShared[3] ? (
+                      <Ionicons name="checkmark-circle" size={32} color="#ffb778"/>
+                    ) : (
+                      <Ionicons name="ellipse-outline" size={32} color="#ffb778"/>
+                    )}
+                  </Pressable>
+                </View>
+              </View>
+
+              {/* Cancel/save new item */}
+              <View style={styles.rowAlignment}>
+                <TouchableOpacity
+                  style={styles.cancelButton}
+                  onPress={() => {
+                    closeWindow();
+                  }}
+                >
+                  <Text style={styles.cancelButtonText}>Cancel</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity 
+                  style={styles.saveButton}
+                  onPress={() => {
+                    addItem();
+                  }}
+                >
+                  <Text style={styles.saveButtonText}>Save</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </Modal>
 
         <Text style={styles.category}>MEAT, POULTRY & SEAFOOD</Text>
         {items.map((item) => (
           <View key={item.id}>
             <View style={styles.line}></View>
-            <PantryItem name={item.name} quantity={item.quantity} unit={item.unit} />
+            <PantryItem id={item.id} name={item.name} quantity={item.quantity} unit={item.unit} expiration={item.expiration} shared={item.shared}/>
           </View>
         ))}
         
@@ -95,5 +264,123 @@ const styles = StyleSheet.create({
   line: {
     borderBottomWidth: 1,
     borderColor: 'lightgray',
+  },
+  windowAlignment: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  window: {
+    marginHorizontal: 50,
+    borderRadius: 8,
+    padding: 35,
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  windowTitle: {
+    fontSize: 24,
+    fontWeight: 600,
+    marginBottom: 30,
+  },
+  windowText: {
+    fontSize: 16,
+  },
+  nameContainer: {
+    marginBottom: 25,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  nameInput: {
+    width: 135,
+    borderWidth: 1,
+    borderRadius: 8,
+    borderColor: 'lightgray',
+    padding: 10,
+    fontSize: 16,
+  },
+  quantityContainer: {
+    marginBottom: 25,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  quantityInput: {
+    width: 70,
+    borderWidth: 1,
+    borderRadius: 8,
+    borderColor: 'lightgray',
+    padding: 10,
+    fontSize: 16,
+  },
+  expirationContainer: {
+    marginBottom: 25,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  expirationInput: {
+    borderWidth: 1,
+    borderRadius: 8,
+    borderColor: 'lightgray',
+    padding: 10,
+  },
+  scrollerSpacer: {
+    flex: 1,
+  },
+  scroller: {
+    paddingBottom: 30,
+    alignItems: 'center',
+    backgroundColor: 'gray',
+  },
+  doneButtonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    backgroundColor: 'gray',
+  },
+  doneButtonText: {
+    marginTop: 15,
+    marginHorizontal: 25,
+    color: '#2fb1ff',
+    fontSize: 20,
+    fontWeight: 600,
+  },
+  sharedSpacer: {
+    marginBottom: 15,
+    alignItems: 'center',
+  },
+  sharedContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 15,
+  },
+  rowAlignment: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  cancelButton: {
+    marginRight: 35,
+    width: 90,
+    borderWidth: 2,
+    borderRadius: 8,
+    borderColor: "#ff8667",
+    alignItems: 'center',
+    paddingVertical: 10,
+    backgroundColor: 'white',
+  },
+  cancelButtonText: {
+    color: '#ff8667',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  saveButton: {
+    width: 90,
+    borderWidth: 2,
+    borderRadius: 8,
+    borderColor: "#ff8667",
+    alignItems: 'center',
+    paddingVertical: 10,
+    backgroundColor: '#ff8667',
+  },
+  saveButtonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
