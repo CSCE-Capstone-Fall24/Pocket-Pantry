b1ff779bbe15c229ed45e524e02cd21eee0f2d06
njck2001 <njck2001@gmail.com>
2024-11-24 20:17:19 -0600

    Committing so that I can checkout into Zack and merge his changes in

--- react/Pocket-Pantry/app/(tabs)/index.tsx
+++ react/Pocket-Pantry/app/(tabs)/index.tsx
@@ -10,20 +10,24 @@ import 'react-native-get-random-values';
 import { v4 as uuidv4 } from 'uuid';
 
 export default function Meal () {
-  /*const [items, setItems] = useState([
+  const [meals, setMeals] = useState([
     { id: "1", name: "Meal1", servings: 1, date: new Date(),
       shared: [false, false, false, false], roommates: ["Roommie 1", "Roommie 2", "Roommie 3", "Roommie 4"],
-      ingredients: ["apple", "orange", "tomato"], ingredient_units: ["lbs", "oz", "purple"], ingredient_quantities: [1, 2, 3] },
+      ingredients: ["apple", "orange", "tomato"], ingredientUnits: ["lbs", "oz", "purple"], ingredientQuantities: [1, 2, 3], 
+      cookTime: 15, recipe: "There is no recipe, figure it out yourself" },
     { id: "2", name: "Meal2", servings: 2, date: new Date(),
       shared: [false, false, false, false], roommates: ["Roommie 1", "Roommie 2", "Roommie 3", "Roommie 4"],
-      ingredients: ["apple", "orange", "tomato"], ingredient_units: ["lbs", "oz", "purple"], ingredient_quantities: [1, 2, 3] },
+      ingredients: ["apple", "orange", "tomato"], ingredientUnits: ["lbs", "oz", "purple"], ingredientQuantities: [1, 2, 3], 
+      cookTime: 15, recipe: "There is no recipe, figure it out yourself" },
     { id: "3", name: "Meal3", servings: 3, date: new Date(),
       shared: [false, false, false, false], roommates: ["Roommie 1", "Roommie 2", "Roommie 3", "Roommie 4"],
-      ingredients: ["apple", "orange", "tomato"], ingredient_units: ["lbs", "oz", "purple"], ingredient_quantities: [1, 2, 3] },
+      ingredients: ["apple", "orange", "tomato"], ingredientUnits: ["lbs", "oz", "purple"], ingredientQuantities: [1, 2, 3], 
+      cookTime: 15, recipe: "There is no recipe, figure it out yourself" },
     { id: "4", name: "Meal4", servings: 4, date: new Date(),
       shared: [false, false, false, false], roommates: ["Roommie 1", "Roommie 2", "Roommie 3", "Roommie 4"],
-      ingredients: ["apple", "orange", "tomato"], ingredient_units: ["lbs", "oz", "purple"], ingredient_quantities: [1, 2, 3] }
-  ]);*/
+      ingredients: ["apple", "orange", "tomato"], ingredientUnits: ["lbs", "oz", "purple"], ingredientQuantities: [1, 2, 3], 
+      cookTime: 15, recipe: "There is no recipe, figure it out yourself" }
+  ]);
 
   interface Meal {
     id: string;
@@ -39,20 +43,18 @@ export default function Meal () {
     recipe: string;
     deleteMeal: (id: string) => void;
   }
-  const [meals, setMeals] = useState<Meal[]>([]);
+  const [plannedMeals, setPlannedMeals] = useState<Meal[]>([]);
 
   {/* Functions - search meal window */}
   const [isWindowVisible, setWindowVisible] = useState(false);
   const openWindow = () => setWindowVisible(true);
   const closeWindow = () => {
-    // We'll need to tie this in with our meal search algorithm
-    setSearch('');
     setWindowVisible(false);
-    setNewName('');
-    setNewServings('');
-    setNewDate(new Date());
-    setNewShared([false, false, false, false]);
+    setSearch('');
+    // Then we need to pull in recipes from database using meal-search algorithm
   };
+
+  {/* Functions - search meal */}
   const [newSearch, setSearch] = useState('');
 
   {/* Functions - new meal name */}
@@ -123,185 +125,6 @@ export default function Meal () {
           <Text style={styles.tempSubtitle}>
             (Frontend Branch) {/* This will need to be removed */}
           </Text>
-          <TouchableOpacity style={styles.addButton} onPress={openWindow}>
-            <Ionicons name="add-outline" size={40} color="white"/>
-          </TouchableOpacity>
-        </View>
-
-        {/* Add meal window */}
-        <Modal
-          transparent={true}
-          animationType="fade"
-          visible={isWindowVisible}
-          onRequestClose={closeWindow}
-        >
-          <BlurView
-            style={StyleSheet.absoluteFill}
-            intensity={20}
-          />
-          <View style={styles.windowAlignment}>
-            <View style={styles.windowContainer}>
-              <Text style={styles.windowTitle}>Add Meal</Text>
-              
-              {/* Search Bar */}
-              <View style={styles.mealSearchBarOuter}>
-                <TextInput
-                  style={styles.mealSearchBarInner}
-                  placeholder = "Search for meals" // This should maybe be gray to distinguish it from actual text inputs
-                  placeholderTextColor = "black"
-                  onChangeText={(value) => setSearch(value)}
-                />
-                <Ionicons name="search-outline" size={40} color="#ff8667"/>
-              </View>
-
-              {/* --Input-servings-- User should select meal and THEN input serving */}
-              <View style={styles.fieldContainer}>
-                <Text style={styles.fieldText}>Servings:  </Text>
-                <TextInput
-                  style={styles.nameInput}
-                  value={newServings}
-                  onChangeText={(value) => setNewServings(value)}
-                />
-              </View>
-
-              {/* Input date to cook meal*/}
-              <View style={styles.fieldContainer}>
-                <Text style={styles.fieldText}>Date to cook meal:  </Text>
-                <TouchableOpacity style={styles.pickerInput} onPress={openDatePicker}>
-                  <Text style={styles.fieldText}>{newDate.toLocaleDateString()}</Text>
-                </TouchableOpacity>
-              </View>
-
-              {/* Date to cook picker */}
-              <Modal
-                transparent={true}
-                animationType="slide"
-                visible={isDatePickerVisible}
-                onRequestClose={closeDatePicker}
-              >
-                <Pressable style={styles.pickerSpacer} onPress={closeDatePicker}></Pressable>
-                <View>
-                  <View style={styles.doneButtonContainer}>
-                    <TouchableOpacity onPress={closeDatePicker}>
-                      <Text style={styles.doneButtonText}>Done</Text>
-                    </TouchableOpacity>
-                  </View>
-                  <View style={styles.pickerB}>
-                    {isDatePickerVisible && (
-                      <DateTimePicker
-                        value={newDate}
-                        mode="date"
-                        display="spinner"
-                        onChange={(event, date) => {if (date) setNewDate(date);}}
-                      />
-                    )}
-                  </View>
-                </View>
-              </Modal>
-
-              {/* Set new meal as shared */}
-              <View style={styles.sharedSpacer}>
-                <View style={styles.sharedContainer}>
-                  <Text style={styles.fieldText}>Shared with {roommates[0]}:  </Text>
-                  <Pressable onPress={() => sharedToggle(0)}>
-                    {newShared[0] ? (
-                      <Ionicons name="checkmark-circle" size={32} color="#e167a4"/>
-                    ) : (
-                      <Ionicons name="ellipse-outline" size={32} color="#e167a4"/>
-                    )}
-                  </Pressable>
-                </View>
-
-                <View style={styles.sharedContainer}>
-                  <Text style={styles.fieldText}>Shared with {roommates[1]}:  </Text>
-                  <Pressable onPress={() => sharedToggle(1)}>
-                    {newShared[1] ? (
-                      <Ionicons name="checkmark-circle" size={32} color="#f4737e"/>
-                    ) : (
-                      <Ionicons name="ellipse-outline" size={32} color="#f4737e"/>
-                    )}
-                  </Pressable>
-                </View>
-
-                <View style={styles.sharedContainer}>
-                  <Text style={styles.fieldText}>Shared with {roommates[2]}:  </Text>
-                  <Pressable onPress={() => sharedToggle(2)}>
-                    {newShared[2] ? (
-                      <Ionicons name="checkmark-circle" size={32} color="#ff8667"/>
-                    ) : (
-                      <Ionicons name="ellipse-outline" size={32} color="#ff8667"/>
-                    )}
-                  </Pressable>
-                </View>
-
-                <View style={styles.sharedContainer}>
-                  <Text style={styles.fieldText}>Shared with {roommates[3]}:  </Text>
-                  <Pressable onPress={() => sharedToggle(3)}>
-                    {newShared[3] ? (
-                      <Ionicons name="checkmark-circle" size={32} color="#ffb778"/>
-                    ) : (
-                      <Ionicons name="ellipse-outline" size={32} color="#ffb778"/>
-                    )}
-                  </Pressable>
-                </View>
-              </View>
-
-              {/* Cancel/save new item */}
-              <View style={styles.rowAlignment}>
-                <TouchableOpacity style={styles.cancelButton} onPress={() => { closeWindow(); }}>
-                  <Text style={styles.cancelButtonText}>Cancel</Text>
-                </TouchableOpacity>
-
-                <TouchableOpacity style={styles.saveButton} onPress={() => { addMeal(); }}>
-                  <Text style={styles.saveButtonText}>Save</Text>
-                </TouchableOpacity>
-              </View>
-            </View>
-          </View>
-        </Modal>
-
-        <Text style={styles.date}>MEAT, POULTRY & SEAFOOD</Text>
-        {meals.map((meal) => (
-          <View key={meal.id}>
-            <View style={styles.line}></View>
-            <MealItem id={meal.id} name={meal.name} servings={meal.servings} date={meal.date}
-              shared={meal.shared} roommates={meal.roommates}
-              ingredients={meal.ingredients} ingredientUnits={meal.ingredientUnits} ingredientQuantities={meal.ingredientQuantities}
-              cookTime={meal.cookTime} recipe={meal.recipe}
-              deleteMeal={deleteMeal}/>
-          </View>
-        ))}
-
-        {/* {categorizedItems.map((categoryGroup) => (
-          categoryGroup.items.length > 0 && (
-            <View key={categoryGroup.category}>
-              <Text style={styles.category}>
-                {categoriesB[categoriesA.indexOf(categoryGroup.category)]}
-              </Text>
-              {categoryGroup.items.map((item) => (
-                <View key={item.id}>
-                  <View style={styles.line}></View>
-                  <PantryItem
-                    id={item.id}
-                    name={item.name}
-                    category={item.category}
-                    quantity={item.quantity}
-                    unit={item.unit}
-                    expiration={item.expiration}
-                    shared={item.shared}
-                    roommates={item.roommates}
-                    deleteItem={deleteItem}
-                  />
-                </View>
-              ))}
-            </View>
-          )
-        ))} */}
-        
-      </SafeAreaView>
-    </ScrollView>
-  );
-}
 
 const styles = StyleSheet.create({
   header: {
